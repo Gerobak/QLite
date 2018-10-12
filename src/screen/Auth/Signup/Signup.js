550734499c0c81fe2f1798b5a19f993ff0b40cc6
Heru6664 <herujulyanto999@gmail.com>
2018-10-12 14:12:43 +0700

    update signup screen

--- src/screen/Auth/Signup/Signup.js
+++ src/screen/Auth/Signup/Signup.js
@@ -1,7 +1,20 @@
 import React, { Component } from "react";
-import { Icon, Container, Content, Text } from "native-base";
-import { View, TouchableOpacity, Image } from "react-native";
-import globalStyles from "../../../assets/styles/global";
+import {
+  Icon,
+  Container,
+  Content,
+  Text,
+  Form,
+  Item,
+  Input,
+  Button,
+  Spinner
+} from "native-base";
+import { View, TouchableOpacity } from "react-native";
+import { CheckBox } from "react-native-elements";
+import globalStyles, { height, colors } from "../../../assets/styles/global";
+import styles from "./SignupStyle";
+import { connect } from "react-redux";
 
 export class Signup extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -15,17 +28,106 @@ export class Signup extends Component {
       </View>
     )
   });
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      phone: "",
+      terms: false,
+      errors: []
+    };
+  }
+
+  onChangeValue = (key, val) => {
+    this.setState({
+      [key]: val
+    });
+  };
   render() {
     return (
       <Container>
         <Content>
           <View style={globalStyles.centerContainer}>
             <Icon
-              style={{ fontSize: 150 }}
+              style={{ fontSize: 100 }}
               name="minecraft"
               type="MaterialCommunityIcons"
             />
-            <Text>Signup</Text>
+            <View
+              style={{
+                width: "85%",
+                paddingVertical: 5
+              }}>
+              <Form>
+                <Item rounded style={styles.formInput}>
+                  <Input
+                    placeholder="First Name"
+                    placeholderTextColor={colors.DARK_BLUE}
+                    onChangeText={val => this.onChangeValue("firstName", val)}
+                  />
+                </Item>
+                <Item rounded style={styles.formInput}>
+                  <Input
+                    placeholder="Last Name"
+                    placeholderTextColor={colors.DARK_BLUE}
+                    onChangeText={val => this.onChangeValue("lastName", val)}
+                  />
+                </Item>
+                <Item rounded style={styles.formInput}>
+                  <Input
+                    placeholder="Email"
+                    placeholderTextColor={colors.DARK_BLUE}
+                    onChangeText={val => this.onChangeValue("email", val)}
+                  />
+                </Item>
+                <Item rounded style={styles.formInput}>
+                  <Input
+                    placeholder="Password"
+                    placeholderTextColor={colors.DARK_BLUE}
+                    onChangeText={val => this.onChangeValue("password", val)}
+                  />
+                </Item>
+                <Item rounded style={styles.formInput}>
+                  <Input
+                    placeholder="Re-Type Password"
+                    placeholderTextColor={colors.DARK_BLUE}
+                    onChangeText={val =>
+                      this.onChangeValue("passwordConfirmation", val)
+                    }
+                  />
+                </Item>
+                <Item rounded style={styles.formInput}>
+                  <Input
+                    placeholder="+62"
+                    placeholderTextColor={colors.DARK_BLUE}
+                    onChangeText={val => this.onChangeValue("phone", val)}
+                  />
+                </Item>
+                <CheckBox
+                  title="Accept Terms & Condition"
+                  checked={this.state.terms}
+                  onPress={() => this.setState({ terms: !this.state.terms })}
+                />
+                <Button
+                  onPress={() => this.handleSignUp()}
+                  disabled={!this.state.terms}
+                  style={[
+                    styles.formInput,
+                    !this.state.terms ? styles.btnDisable : styles.btnLogin
+                  ]}>
+                  {this.props.isLoading ? (
+                    <Spinner color="white" />
+                  ) : (
+                    <Text style={globalStyles.font}>SIGNUP</Text>
+                  )}
+                </Button>
+              </Form>
+            </View>
           </View>
         </Content>
       </Container>
@@ -33,4 +135,16 @@ export class Signup extends Component {
   }
 }
 
-export default Signup;
+const mapStateToProps = ({ auth }) => ({
+  isLoading: auth.isLoading,
+  errorMessage: auth.errorMessage
+});
+
+const mapDispatchToProps = dispatch => ({
+  loginAuth: user => dispatch(loginAuth(user))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Signup);
